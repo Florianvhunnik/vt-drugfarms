93eaa8507693aa1f9184b5cc479bc6d2d5741061
Florian <florian2006email@gmail.com>
2024-09-25 14:13:40 +0200

    Remove quotes

--- client/main.lua
+++ client/main.lua
@@ -19,30 +19,23 @@ end)
 Setup = function(action)
     local success, result = pcall(function()
         if action == "stop" then
-            -- delete all props and remove all blips
             DeleteProps(); RemoveBlips()
 
-            -- remove all polyzones
             for k, v in pairs(polyZones) do
                 exports.ox_target:removeZone(k)
             end
 
-            -- remove all targets for each prop type
             for k, v in pairs(Config.drugFarms) do
                 exports.ox_target:removeModel(v.target.propType)
             end
 
-            -- clear the tables
             propData, polyZones, blips, targets = {}, {}, {}, {}
         elseif action == "restart" then
-            -- stop and start the resource
             Setup("stop")
             DebugHandler('info', 'Restarting...');
             Wait(1000); Setup("start")
         elseif action == "start" then
-            -- loop through all drug types
             for k, v in pairs(Config.drugFarms) do
-                -- add the target for the prop type
                 if not targets[v.target.propType] then
                     exports.ox_target:addModel(v.target.propType, {
                         {
@@ -57,13 +50,11 @@ Setup = function(action)
                     }); targets[v.target.propType] = true
                 end
 
-                -- create the blip if enabled, then calculate the coords
                 if v.blip.enabled then
                     local center = CalculateCentroid(v.zone)
                     CreateBlip(k, center, v.blip.sprite, v.blip.color, v.blip.scale, v.blip.label)
                 end
 
-                -- create the polyzone
                 polyZones[k] = lib.zones.poly({
                     points = v.zone,
                     thickness = 2,
@@ -92,7 +83,6 @@ end
 
 DebugHandler = function(type, message)
     if not Config.debugMode then return end
-    -- check the type, then change the message
     if type == 'info' then
         message = "^5[INFO]^7 " .. message
     elseif type == 'error' then
@@ -103,11 +93,10 @@ DebugHandler = function(type, message)
         message = "^2[SUCCESS]^7 " .. message
     else
         return false
-    end; print('^6[DEBUG]^7 ' .. message) -- print the message
+    end; print('^6[DEBUG]^7 ' .. message)
 end
 
 CreateBlip = function(k, coords, sprite, color, scale, label)
-    -- create the blip and add it to the blips table so we can remove it later
     blips[k] = AddBlipForCoord(coords)
     SetBlipSprite(blips[k], sprite)
     SetBlipColour(blips[k], color)
@@ -119,15 +108,12 @@ CreateBlip = function(k, coords, sprite, color, scale, label)
 end
 
 RemoveBlips = function()
-    -- create a pcall function to catch any errors
     local success, result = pcall(function()
-        -- loop through all blips and remove them, then clear the table
         for k, blip in pairs(blips) do
             RemoveBlip(blip)
-        end; blips = {} -- clear the blips table
+        end; blips = {}
     end)
 
-    -- check if the removal was successful
     if success then
         DebugHandler("success", "All blips have been removed successfully.")
     else
@@ -137,7 +123,7 @@ end
 
 OnEnter = function(self, k)
     DebugHandler('info', 'Entered drugs zone: ^2' .. k)
-    activeZone = k -- set the active zone
+    activeZone = k
 
     local drugsFarm = Config.drugFarms[activeZone]
     for i = 1, drugsFarm.spawnLimit do
@@ -147,32 +133,24 @@ OnEnter = function(self, k)
 end
 
 CalculateCentroid = function(points)
-    local numPoints = #points -- get the point count
-    local sumX, sumY, sumZ = 0, 0, 0 -- give x,y,z a v of 0
+    local numPoints = #points
+    local sumX, sumY, sumZ = 0, 0, 0
 
-    -- loop trough all points and calculate the sum
     for k, point in ipairs(points) do
         sumX, sumY, sumZ = sumX + point.x, sumY + point.y, sumZ + point.z
     end
 
-    -- return the vector we calculated
     return vec(sumX / numPoints, sumY / numPoints, sumZ / numPoints)
 end
 
 SpawnProp = function(propType)
-    -- get the drug farm config
     drugFarm = Config.drugFarms[activeZone]
 
-    -- check if the drug farm is not nil
     if drugFarm then
-        -- get the spawn coords
         local coords = GetSpawnCoords(propType, drugFarm.zone)
 
-        -- check if the coords are not nil
         if coords then
-            -- create a pcall function to catch any errors
             local success, result = pcall(function()
-                -- spawn, freeze and place the prop on the ground
                 ESX.Game.SpawnLocalObject(propType, coords, function(object)
                     PlaceObjectOnGroundProperly(object)
                     FreezeEntityPosition(object, true)
@@ -180,7 +158,6 @@ SpawnProp = function(propType)
                 end)
             end)
 
-            -- check if the prop was spawned successfully
             if success then
                 DebugHandler('info', 'Spawned prop: ^2' .. propType .. '^7 in zone: ^2' .. activeZone)
             else
@@ -219,15 +196,12 @@ GetSpawnCoords = function(propType, zone)
 end
 
 PickupProp = function(data)
-    -- check if the player is already interacting with something
     if interactActive then return end
 
-    -- change some things
     interactActive, LocalPlayer.state.invBusy = true, true
     TaskTurnPedToFaceEntity(PlayerPedId(), data.entity, 1000)
     Wait(1000)
 
-    -- start the progress circle
     local progress = lib.progressCircle({
         duration = 5000,
         position = 'bottom',
@@ -240,27 +214,20 @@ PickupProp = function(data)
         },
     })
 
-    -- check if the progress was completed
     if progress then
         local prop = data.entity
-
-        -- check if the prop exists
         if propData[prop] then
             local propType = propData[prop]
-            propData[prop] = nil        -- remove the prop from the table
-            ESX.Game.DeleteObject(prop) -- delete the prop
-
-            -- spawn a new prop
+            propData[prop] = nil
+            ESX.Game.DeleteObject(prop)
             SpawnProp(propType)
         end
     end
 
-    -- change some things back
     interactActive, LocalPlayer.state.invBusy = false, false
 end
 
 DeleteProps = function()
-    -- loop through all props and delete them
     for prop in pairs(propData) do
         if DoesEntityExist(prop) then
             ESX.Game.DeleteObject(prop)
@@ -268,11 +235,10 @@ DeleteProps = function()
             DebugHandler('warning', 'The prop does not exist.')
         end
     end; DebugHandler('info', 'Deleted ^2' .. #propData .. '^7 props.')
-    propData = {} -- clear the propData table
+    propData = {}
 end
 
 InDrugsFarm = function()
-    -- loop through all polyzones and check if the player is inside one
     for k, v in pairs(polyZones) do
         if v:isPointInside(GetEntityCoords(PlayerPedId())) then
             return k

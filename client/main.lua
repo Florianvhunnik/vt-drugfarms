4cb5bf34b4702a27295759e54918c376af4fcbd1
Florian <florian2006email@gmail.com>
2024-09-30 14:17:25 +0200

    Make some test commands

--- client/main.lua
+++ client/main.lua
@@ -13,3 +13,21 @@ end)
 -- Exports
 exports('InDrugsFarm', InDrugsFarm)
 exports('GetActiveZone', GetActiveZone)
+
+-- Test commands
+RegisterCommand('InDrugsFarm', function()
+    if exports[GetCurrentResourceName()]:InDrugsFarm() then
+        print('You are in a drug farm')
+    else
+        print('You are not in a drug farm')
+    end
+end)
+
+RegisterCommand('GetActiveZone', function()
+    local zone = exports[GetCurrentResourceName()]:GetActiveZone()
+    if zone then
+        print('You are in zone: ' .. zone)
+    else
+        print('You are not in a zone')
+    end
+end)
--- configuration/config.lua
+++ configuration/config.lua
@@ -36,4 +36,36 @@ Config.drugFarms = {
             vec(112.8641, -978.5941, 29.4073),
         }
     },
+    ["Weed Field1"] = {
+        spawnLimit = 10,
+        harvestAnimation = {
+            dict = "anim@amb@clubhouse@tutorial@bkr_tut_ig3@",
+            clip = "machinic_loop_mechandplayer",
+        },
+        blip = {
+            enabled = true,
+            label = "Weed Field",
+            sprite = 496,
+            color = 25,
+            scale = 0.8,
+        },
+        item = {
+            spawnName = "weed",
+            randomized = {
+                min = 1,
+                max = 3,
+            },
+        },
+        target = {
+            propType = "prop_weed_01",
+            icon = "fas fa-cannabis",
+            label = "Harvest weed",
+            distance = 2.0,
+        },
+        zone = {
+            vec(122.0877, -1016.0814, 29.3637),
+            vec(129.5682, -995.9285, 29.3030),
+            vec(142.9541, -999.5405, 29.2557),
+        }
+    },
 }

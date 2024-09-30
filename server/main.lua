67b77e0738a0b328f80d16b700fe5cf908442c3d
Florian <florian2006email@gmail.com>
2024-09-30 08:56:45 +0200

    Move server side functions to new file

--- server/main.lua
+++ server/main.lua
@@ -10,19 +10,3 @@ AddEventHandler('onResourceStop', function(resource)
         StopResource(GetCurrentResourceName())
     end
 end)
-
--- functions
-DebugHandler = function(type, message)
-    if not Config.debugMode then return end
-    if type == 'info' then
-        message = "^5[INFO]^7 " .. message
-    elseif type == 'error' then
-        message = "^1[ERROR]^7 " .. message
-    elseif type == 'warning' then
-        message = "^3[WARNING]^7 " .. message
-    elseif type == 'success' then
-        message = "^2[SUCCESS]^7 " .. message
-    else
-        return false
-    end; print('^6[DEBUG]^7 ' .. message)
-end
